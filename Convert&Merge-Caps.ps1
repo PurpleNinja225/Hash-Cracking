e5e5e130a98aa5d0e11397d147a9a229c29b6cd0
PurpleNinja225 <haydenlink225@gmail.com>
2018-08-12 10:50:15 -0800

    Initial Upload

---
+++ Convert&Merge-Caps.ps1
@@ -0,0 +1,12 @@
+﻿$files = Get-ChildItem -name new_caps
+
+foreach ($file in $files){
+    $file2 = $file
+    .\cap2hccapx.exe new_caps\$file new_hccapxs\$file2.hccapx
+    }
+
+echo "Done converting. Now merging hccapx files together"
+
+Start-Sleep 1
+
+gc -encoding Byte -path new_hccapxs\*.hccapx | sc -Encoding Byte multi_hccapxs\multi.hccapx
