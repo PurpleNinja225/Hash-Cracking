0080575927005508a866c1cb7c68c6aeaabb3392
PurpleNinja225 <haydenlink225@gmail.com>
2018-08-12 10:45:40 -0800

    Initial Upload

---
+++ Hashcat-Batch-Attack.ps1
@@ -0,0 +1,16 @@
+﻿
+Param(
+    [string]$PathToHashcatExE,     #Path to your hashcat executable.
+    [string]$AttackMode = "0",     #Attack mode: Stright, Combination, Brute-force, etc..
+    [string]$DeviceType = "2",     #Device used for cracking: CPU, GPU, etc..
+    [string]$HashMode,             #Number for your attack mode IE: 0, 100, 3200 (see https://hashcat.net/wiki/doku.php?id=hashcat for list of attack modes)
+    [string]$OutFile,              #Location for hashcat to output found hashes and passwords.
+    [string]$PathToFolder,         #Path to the folder that contains all of the hashfiles you want to run against.
+    [string]$PathToWordlist        #Path to wordlist(s) you want to attack against. Multiple wordlists are supported.
+    )
+
+$HashFile = Get-ChildItem -File $PathToFolder
+
+foreach ($File in $HashFile){
+    &$PathToHashcatExE --attack-mode $AttackMode --opencl-device-types $DeviceType --hash-type $HashMode --workload-profile 3 --status --status-timer=60 --remove --separator : --gpu-temp-disable --outfile $OutFile --outfile-format=3 $File.FullName $PathToWordList
+    }
