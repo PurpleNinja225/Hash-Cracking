d20f53dc3290deb1aa96c8f2669957da7366bcc9
PurpleNinja225 <haydenlink225@gmail.com>
2018-08-12 11:10:42 -0800

    Added comments

--- Convert&Merge-Caps.ps1
+++ Convert&Merge-Caps.ps1
@@ -1,4 +1,8 @@
-﻿$files = Get-ChildItem -name new_caps
+#Written by PurpleNinja225
+#This script is used to search a directory for cap files, convert them to hccapx format then merge the caps into one file that can be used by Hashcat.
+#This script *does not clean up cap files* it will only append one to the end of another.
+
+$files = Get-ChildItem -name new_caps
 
 foreach ($file in $files){
     $file2 = $file
@@ -6,7 +10,6 @@ foreach ($file in $files){
     }
 
 echo "Done converting. Now merging hccapx files together"
-
 Start-Sleep 1
 
 gc -encoding Byte -path new_hccapxs\*.hccapx | sc -Encoding Byte multi_hccapxs\multi.hccapx
