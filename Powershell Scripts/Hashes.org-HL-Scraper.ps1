9589ccda1aaf6e86895b0f67b03f60ae4db68bc8
PurpleNinja225 <haydenlink225@gmail.com>
2018-12-24 23:42:54 -0900

    simple webscraper for hashes.org found lists

---
+++ Powershell Scripts/Hashes.org-HL-Scraper.ps1
@@ -0,0 +1,7 @@
+﻿ $A = 14 .. 1150
+ $i = 14
+foreach( $i in $A) {
+    Invoke-WebRequest -Uri "https://hashes.org/download.php?type=hfound&hashlistId=$i" -Headers @{"Referer"="https://hashes.org/"} -O "D:\hashes.org leaks\$i.txt";
+    
+       $i++
+        }
---
+++ Powershell Scripts/copy -saltedhashes.ps1
@@ -0,0 +1,6 @@
+﻿Foreach ($line in [System.IO.File]::ReadLines('C:\Users\Desktop\Downloads\hashes.org_salted_left_001_ULM_002.txt'))
+    {
+    copy-item "F:\Original dumps (untouched)\$line* .txt" -Destination "F:\salted dumps\"
+
+
+    }
